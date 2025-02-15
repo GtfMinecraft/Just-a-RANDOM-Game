2ca057f4a3ac2736d1a696ed9987a2be123179b6
Gtf <92994335+GtfMinecraft@users.noreply.github.com>
2025-02-15 13:55:09 +0800

    Moon Halo & Smoother Skybox Transition

--- Just a RANDOM Game/Assets/Scripts/DayNightCycle.cs
+++ Just a RANDOM Game/Assets/Scripts/DayNightCycle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour, IDataPersistence
@@ -22,7 +23,7 @@ public class DayNightCycle : MonoBehaviour, IDataPersistence
     public Color nightLightColor;
 
     private bool isNight = false;
-    private float lightChangeSpeed;
+    private float skyboxBlend = 0;
 
     private void Awake()
     {
@@ -39,7 +40,27 @@ public class DayNightCycle : MonoBehaviour, IDataPersistence
     void Start()
     {
         moon.localScale = Vector3.one * moonScale;
-        lightChangeSpeed = 60 / (dayLightIntensity - nightLightIntensity);
+
+        if ((inGameTime < 6 || inGameTime > 18))
+        {
+            RenderSettings.skybox = nightSkybox;
+            isNight = true;
+            directionalLight.intensity = nightLightIntensity;
+            directionalLight.color = nightLightColor;
+            skyboxBlend = 1;
+            nightSkybox.SetFloat("_Exposure", 1.3f);
+            daySkybox.SetFloat("_Exposure", 0.1f);
+        }
+        else
+        {
+            RenderSettings.skybox = daySkybox;
+            isNight = false;
+            directionalLight.intensity = dayLightIntensity;
+            directionalLight.color = dayLightColor;
+            skyboxBlend = 0;
+            nightSkybox.SetFloat("_Exposure", 0.2f);
+            daySkybox.SetFloat("_Exposure", 1.3f);
+        }
     }
 
     // Update is called once per frame
@@ -53,36 +74,63 @@ public class DayNightCycle : MonoBehaviour, IDataPersistence
             ++gameDays;
         }
 
-        if (isNight && directionalLight.intensity > nightLightIntensity)
-            directionalLight.intensity -= Time.deltaTime / lightChangeSpeed;
-        else if (!isNight && directionalLight.intensity < dayLightIntensity)
-            directionalLight.intensity += Time.deltaTime / lightChangeSpeed;
+        if (isNight && skyboxBlend < 1)
+        {
+            skyboxBlend += Time.deltaTime / 6;
+            if (skyboxBlend <= 0.5)
+            {
+                directionalLight.intensity = Mathf.Lerp(dayLightIntensity, 0, skyboxBlend * 2);
+                float exposure = Mathf.Lerp(1.3f, 0.1f, skyboxBlend * 2);
+                daySkybox.SetFloat("_Exposure", exposure);
+            }
+            else
+            {
+                directionalLight.intensity = Mathf.Lerp(0, nightLightIntensity, skyboxBlend * 2 - 1);
+                if (RenderSettings.skybox != nightSkybox)
+                    RenderSettings.skybox = nightSkybox;
+                float exposure = Mathf.Lerp(0.2f, 1.3f, skyboxBlend * 2 - 1);
+                nightSkybox.SetFloat("_Exposure", exposure);
+            }
+
+            directionalLight.color = Color.Lerp(dayLightColor, nightLightColor, skyboxBlend);
+        }
+        else if (!isNight && skyboxBlend > 0)
+        {
+            skyboxBlend -= Time.deltaTime / 6;
+            if (skyboxBlend > 0.5)
+            {
+                directionalLight.intensity = Mathf.Lerp(0, nightLightIntensity, skyboxBlend * 2 - 1);
+                float exposure = Mathf.Lerp(0.2f, 1.3f, skyboxBlend * 2 - 1);
+                nightSkybox.SetFloat("_Exposure", exposure);
+            }
+            else
+            {
+                directionalLight.intensity = Mathf.Lerp(dayLightIntensity, 0, skyboxBlend * 2);
+                if (RenderSettings.skybox != daySkybox)
+                    RenderSettings.skybox = daySkybox;
+                float exposure = Mathf.Lerp(1.3f, 0.1f, skyboxBlend * 2);
+                daySkybox.SetFloat("_Exposure", exposure);
+            }
+
+            directionalLight.color = Color.Lerp(dayLightColor, nightLightColor, skyboxBlend);
+        }
 
         moon.rotation = Camera.main.transform.rotation * Quaternion.Euler(-90, 0, 0);
         float xz = Mathf.Cos((inGameTime + 6) / 12 * Mathf.PI);
         moon.position = new Vector3(xz / 2, Mathf.Sin((inGameTime + 6) / 12 * Mathf.PI), -xz * 1.732051f / 2) * moonDistance;
 
-        if((inGameTime < 6 || inGameTime > 18))
-        {
+        if (RenderSettings.skybox == nightSkybox)
             directionalLight.transform.rotation = Quaternion.Euler((inGameTime + 6) * 15, -30, 0);
-
-            if (!isNight)
-            {
-                directionalLight.color = nightLightColor;
-                isNight = true;
-                RenderSettings.skybox = nightSkybox;
-            }
-        }
-        else if(inGameTime >= 6 && inGameTime <= 18)
-        {
+        else if (RenderSettings.skybox == daySkybox)
             directionalLight.transform.rotation = Quaternion.Euler((inGameTime - 6) * 15, -30, 0);
 
-            if (isNight)
-            {
-                directionalLight.color = dayLightColor;
-                isNight = false;
-                RenderSettings.skybox = daySkybox;
-            }
+        if ((inGameTime < 6 || inGameTime > 18) && !isNight)
+        {
+            isNight = true;
+        }
+        else if(inGameTime >= 6 && inGameTime <= 18 && isNight)
+        {
+            isNight = false;
         }
     }
 
